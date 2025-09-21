74c1424b2cef9e67ffd2038399831e26233f569a
haykam821 <24855774+haykam821@users.noreply.github.com>
2025-09-21 00:39:47 -0400

    Update mod to Minecraft 1.21.4

--- src/main/java/agency/highlysuspect/dokokashiradoor/mixin/client/WorldRendererMixin.java
+++ src/main/java/agency/highlysuspect/dokokashiradoor/mixin/client/WorldRendererMixin.java
@@ -3,7 +3,6 @@ package agency.highlysuspect.dokokashiradoor.mixin.client;
 import agency.highlysuspect.dokokashiradoor.client.MatrixCache;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.LightmapTextureManager;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.ObjectAllocator;
@@ -16,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 	@Inject(method = "render", at = @At("HEAD"))
-	private void smuggleMatrices(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci) {
+	private void smuggleMatrices(ObjectAllocator allocator, RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, Matrix4f positionMatrix, Matrix4f projectionMatrix, CallbackInfo ci) {
 		MatrixCache.PROJECTION_MATRIX = projectionMatrix;
 		MatrixCache.VIEW_MATRIX = positionMatrix;
 	}
